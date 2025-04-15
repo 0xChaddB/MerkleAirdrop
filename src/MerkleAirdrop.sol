813a24422a3230cc3c67116b963cecbdd98f61bf
0xChaddB <0xChaddB@proton.me>
2025-04-15 14:02:19 +0300

    feat: basic claim & compilation errors

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,4 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MerkleAirdrop 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+    // some list of addresses 
+    // Allow someone in the list to claime ERC20 Tokens
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    // no for-loops, Merkle proofs better !
+
+    error MerkleAirdrop__InvalidProof();
+    event Claim(address account, uint256 amount);
+    constructor(bytes32 merkleRoot, IERC20 airdropToken){
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // Calculate using the account and the amount; the hash -> lead node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
+            revert MerkleAirdrop__InvalidProof();
+        }
+
+        i_airdropToken.safeTransfer(account, amount);
+        emit Claim(account, amount);
+    }
+}
