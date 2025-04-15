793a9c33343012752f6bc706be1ff943322a88a6
0xChaddB <0xChaddB@proton.me>
2025-04-15 18:27:19 +0300

    feat: Merkle scripts

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -40,4 +40,12 @@ contract MerkleAirdrop {
         emit Claim(account, amount);
         
     }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20){
+        return i_airdropToken;
+    }
 }
---
+++ test/MerkleAirdrop.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+contract MerkleAirdropTest is Test {
+
+
+    
+}
